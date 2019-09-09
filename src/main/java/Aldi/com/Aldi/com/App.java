bb7b92bef2128d9f11efc1ef1e73e3b5df2c6b19
christopher ovie <christophodibo@yahoo.com>
2019-09-09 18:45:00 +0100

    second commit

---
+++ src/main/java/Aldi/com/Aldi/com/App.java
@@ -0,0 +1,13 @@
+package Aldi.com.Aldi.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
